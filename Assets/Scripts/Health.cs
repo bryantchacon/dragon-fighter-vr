88fd89802066bc3ebd32d4a4b56fc4e0351b2b85
bryantchacon <bryantneta@hotmail.com>
2021-08-14 15:04:49 -0500

    Enemigos implementados

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,6 +6,17 @@ public class Health : MonoBehaviour
 {
     public DamageType damageType = DamageType.enemy; //Se configura como enemigo por default pero se puede cambiar en el editor, el tipo de dato viene del enum en Damage
 
+    private Animator characterAnimator;
+
+    void Start()
+    {
+        if (GetComponent<Animator>() != null) //Si el game object que tiene este script tiene un animator... NOTA: GetComponent<> es para buscar un componente dentro del game object donde esta el mismo script
+        {
+            characterAnimator = GetComponent<Animator>(); //Se guarda en la variable characterAnimator...
+            characterAnimator.SetFloat("Health", HealthPoints); //Y setea el valor del parametro Health del animator al de HealthPoints(propiedad de la variable privada de vida) al iniciar el juego
+        }
+    }
+
     //Propiedad para poder manejar la variable privada healthPoints del personaje
     public float HealthPoints
     {
@@ -14,18 +25,19 @@ public class Health : MonoBehaviour
         set //set lo asigna, y puede tener otras funciones
         {
             healthPoints = value; //Asigna a healthPoints el valor que se pase por medio de HealthPoints
+            if (characterAnimator != null)
+            {
+                characterAnimator.SetFloat("Health", healthPoints); //Setea el valor del parametro Health del animator al de healthPoints cuando se setea aqui arriba
+            }
 
             if (healthPoints <= 0) //Si la healthPoints es menor o igual a 0...
             {
-                //TODO: Gestionar la muerte del personaje
+                if (damageType == DamageType.enemy) //Y si el tipo de daño que hace el personaje es enemigo, osea, el personaje es un enemigo...
+                {
+                    Destroy(gameObject, 2.0f); //Se destruira luego de 2 segundos
+                }
             }
         }
     }
     [SerializeField] private float healthPoints = 100.0f; //Tiene 100 como valor por default
-
-
-    void Start()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject[] enemies; //Array de enemigos
+    public int numberOfEnemies; //Numero de enemigos a instanciar por partida
+
+    void Start()
+    {
+        StartCoroutine("GenerateNewEnemy"); //Activacion de la corrutina de generacion de enemigos al iniciar el juego
+    }
+
+    IEnumerator GenerateNewEnemy()
+    {
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            GameObject enemy = enemies[Random.Range(0, enemies.Length)];
+            Instantiate(enemy, transform); //Instancia al enemigo desde el game object que tenga este script
+            yield return new WaitForSeconds(Random.Range(10, 20));
+        }
+    }
+}
